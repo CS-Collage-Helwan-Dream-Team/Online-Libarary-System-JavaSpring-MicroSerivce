bc9c2c8831ec4b1a3dbe1b8caefa6c3e972d8226
Abdulrahman Yasser <abdofahd852@gmail.com>
2024-05-06 23:31:23 +0300

    Solve Database Problems , Add CategoryService

--- book-service/src/main/java/com/example/demo/Controller/BookController.java
+++ book-service/src/main/java/com/example/demo/Controller/BookController.java
@@ -2,10 +2,12 @@ package com.example.demo.Controller;
 
 
 import com.example.demo.Model.Book;
+import com.example.demo.Model.Category;
 import com.example.demo.Repository.BookRepository;
 import com.example.demo.Requests.AddBookRequest;
 import com.example.demo.Requests.UpdateBookRequest;
 import com.example.demo.Service.BookService;
+import com.example.demo.Service.CategoryService;
 import com.example.demo.annotations.UserRoleCheck;
 import com.example.demo.dtos.ResponseDTO;
 import com.example.demo.enums.UserRole;
@@ -24,11 +26,13 @@ import java.util.Optional;
 public class BookController {
     private final BookService bookService;
     private final BookRepository bookRepository;
+    private final CategoryService CategoryService;
 
     @Autowired
-    public BookController(BookService bookService, BookRepository bookRepository) {
+    public BookController(BookService bookService, BookRepository bookRepository , CategoryService CategoryService){
         this.bookService = bookService;
         this.bookRepository = bookRepository;
+        this.CategoryService = CategoryService;
     }
     @AuthorizationRequired
     @UserRoleCheck(UserRole.LIBRARIAN)
@@ -54,6 +58,19 @@ public class BookController {
         );
     }
 
+    @GetMapping("/category")
+    public ResponseEntity<ResponseDTO> getCategories(@RequestHeader("credentials") String credentials) {
+        List<Category> categories = CategoryService.getCategories();
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseDTO(
+                        "Categories found successfully",
+                        HttpStatus.OK,
+                        new HashMap<String , Object>(){{
+                            put("categories", categories);
+                        }}
+                )
+        );
+    }
 
     @GetMapping("/{id}")
     public ResponseEntity<ResponseDTO> getBookById(@PathVariable Integer id,@RequestHeader("credentials") String credentials) {
